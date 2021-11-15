d25b2f75ee61cf8f69ac75a12c879d2fb2a4c0a6
Ian Benedict Famero <ianfamero@gmail.com>
2021-11-15 15:47:46 +0800

    add animations on scroll, fix padding on header title for ipad pro, add meta description and og tags for SEO

---
+++ js/scroll-animation.js
@@ -0,0 +1,25 @@
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      if (entry.target.classList.contains('title-picture')) {
+        entry.target.classList.add('fade-in');
+      } else if (entry.target.classList.contains('front-end-box')) {
+        entry.target.classList.add('fade-in-left');
+      } else if (entry.target.classList.contains('ci-cd-box')) {
+        entry.target.classList.add('fade-in-right');
+      } else {
+        entry.target.classList.add('fade-in-up');
+      }
+    }
+  });
+});
+
+// observed classes
+observer.observe(document.querySelector('.title-picture'));
+observer.observe(document.querySelector('.expertise-content'));
+observer.observe(document.querySelector('.front-end-box'));
+observer.observe(document.querySelector('.back-end-box'));
+observer.observe(document.querySelector('.ci-cd-box'));
+observer.observe(document.querySelector('.technical-skills-content'));
+observer.observe(document.querySelector('.skills-container'));
+
