b60d14cf23983a6b07974512b7d9ccf2b00609c5
Ian Benedict Famero <ianfamero@gmail.com>
2021-11-15 17:30:39 +0800

    add hamburger menu for mobile

--- TODO.md
+++ TODO.md
@@ -3,7 +3,6 @@
 Vanilla HTML, CSS, and JavaScript version of my website
 
 ### Todo
-- [ ] Create hamburger menu for mobile
 - [ ] Register this website on Google Webmasters/Search Console
 
 ### In Progress
@@ -13,3 +12,4 @@ Vanilla HTML, CSS, and JavaScript version of my website
 - [x] Add animations on scroll
 - [x] Fix padding on header title text for iPad Pro
 - [x] Add meta title and description for SEO
+- [x] Create hamburger menu for mobile
---
+++ js/script.js
@@ -0,0 +1,18 @@
+const hamburger = document.querySelector('.hamburger');
+const menuList = document.querySelector('.menu-list');
+
+hamburger.addEventListener("click", () => {
+  console.log(hamburger);
+  console.log(menuList);
+  if (menuList.classList.contains('show-menu')) {
+    menuList.classList.remove('show-menu');
+  } else {
+    menuList.classList.add('show-menu');
+  }
+});
+
+window.addEventListener("scroll", (event) => {
+  if(event.target) {
+    menuList.classList.remove('show-menu');
+  }
+});
